591d8945bb6f3e9cfcca6e495c6ea717d496b04c
uzvg <1497911983@qq.com>
2025-02-26 17:54:21 +0800

    edit config fill will 'chezmoi edit --apply'

--- home/private_dot_config/zsh/functions.zsh
+++ home/private_dot_config/zsh/functions.zsh
@@ -1,3 +1,12 @@
 function _show_correct { print -u1 -R "➡  CORRECT: $1" }
 function _show_warning { print -u1 -R "➡  WARNING: $1" }
 function _show_error { print -u2 -R "➡  ERROR: $1" }
+# Edit config file with `chezmoi edit`
+function _chezmoi_edit() {
+  chezmoi edit --apply $1
+	if [ -N $1 ]; then
+	  exec zsh
+		_show_correct "$1 reloaded successfully!"
+	fi
+}
+
