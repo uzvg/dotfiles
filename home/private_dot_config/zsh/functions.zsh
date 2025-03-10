7467d30ff15d2453d8ef2e7405013b99c69714d1
uzvg <1497911983@qq.com>
2025-03-10 22:09:07 +0800

    fix print functions

--- home/private_dot_config/zsh/functions.zsh
+++ home/private_dot_config/zsh/functions.zsh
@@ -1,6 +1,12 @@
-function _show_correct { print -u1 -R "➡  CORRECT: $1" }
-function _show_warning { print -u1 -R "➡  WARNING: $1" }
-function _show_error { print -u2 -R "➡  ERROR: $1" }
+#!/usr/bin/zsh
+
+# 打印回显信息
+# 调整颜色编号（1=红，2=绿，3=黄）
+function _show_correct() { print -u1 -P "%F{green}✓%f %B%F{2}CORRECT:%f%b ${(j: :)@}" }
+function _show_tip() { print -u1 -P "%F{green}✓%f %B%F{2}TIP:%f%b ${(j: :)@}" }
+function _show_warning() { print -u1 -P "%F{yellow}⚠%f %B%F{3}WARNING:%f%b ${(j: :)@}" }
+function _show_error() { print -u2 -P "%F{red}✗%f %B%F{1}ERROR:%f%b ${(j: :)@}" }
+
 # Edit config file with `chezmoi edit`
 function _chezmoi_edit() {
   chezmoi edit --apply $1
