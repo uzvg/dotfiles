848f5105cf43ac76d313b1b12b4b1986f7177787
uzvg <1497911983@qq.com>
2026-04-23 18:02:59 +0800

    [nvim] chore updates

--- home/private_dot_config/nvim/lua/mappings.lua
+++ home/private_dot_config/nvim/lua/mappings.lua
@@ -7,7 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "Q", "<CMD>q<CR>", { desc = "Quit the editor" })
-map("n", "S", "<CMD>wq<CR>", { desc = "Save and quit the editor" })
+map("n", "S", "<CMD>w<CR>", { desc = "Save and quit the editor" })
+map("n", "<leader>nt", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+-- map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
 -- vim.keymap.set('n', 'Q', '<cmd>q<CR>')
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
