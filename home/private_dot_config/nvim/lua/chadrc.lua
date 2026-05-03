e5e676cf710504333b228ea7c4f085ddb4e842ad
uzvg <1497911983@qq.com>
2026-05-03 20:54:18 +0800

    [nvchad] enable italic comment

--- home/private_dot_config/nvim/lua/chadrc.lua
+++ home/private_dot_config/nvim/lua/chadrc.lua
@@ -8,10 +8,10 @@ local M = {}
 M.base46 = {
 	theme = "chadracula",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
 }
 
 -- M.nvdash = { load_on_startup = true }
