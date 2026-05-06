ae8362967678caa53fcbd459460fb06d1fd1c3ae
uzvg <1497911983@qq.com>
2026-05-06 21:17:42 +0800

    [alias] refine

--- home/private_dot_config/zsh/alias.zsh
+++ home/private_dot_config/zsh/alias.zsh
@@ -9,21 +9,21 @@ alias g='lazygit'
 alias c='bat'
 alias t="btop --config $XDG_CONFIG_HOME/btop/btop_proc_only.conf --preset 1"
 alias e="$EDITOR"
-alias vim="$EDITOR"
 # alias hx="helix"
 alias se="sudoedit"
 alias q='exit'
 alias Q='exit'
 alias zdebug='time ZSH_DEBUGRC=1 zsh -i -c exit'
 alias df="duf"
-alias du="dust -X Games"
+# alias du="dust -X Games"
+alias du="dust"
 # alias fd="fd -H -E Games -E ExtendDisk"
 alias udd='update-desktop-database $XDG_DATA_HOME/applications/'
 alias gedit="gnome-text-editor"
 alias macc="macchina -t Helium"
 # gdu but exclude Games disk
-alias gdu="gdu -i $HOME/Games -i $HOME/ExtendDisk"
-
+# alias gdu="gdu -i $HOME/Games -i $HOME/ExtendDisk"
+alias ff="fastfetch"
 
 # ==============================================
 # zellij alias
@@ -36,10 +36,10 @@ alias gdu="gdu -i $HOME/Games -i $HOME/ExtendDisk"
 # ==============================================
 # taskwarrior-tui alias
 # ==============================================
-# alias tt='taskwarrior-tui'
+alias tt='taskwarrior-tui'
 
 # ==============================================
-# Edit and autoreload zsh-related config
+# zsh
 # ==============================================
 alias zshrc="chezmoi_edit '$ZDOTDIR/.zshrc'"
 alias alrc="chezmoi_edit '$ZDOTDIR/alias.zsh'"
@@ -47,7 +47,7 @@ alias envrc="chezmoi_edit '$ZDOTDIR/env.zsh'"
 alias zimrc="chezmoi_edit $ZDOTDIR/.zimrc"
 
 # ----------------------------------------------
-# other chezoi-edited config files
+# chezmoi edit
 # ----------------------------------------------
 # fontconfig
 alias ftrc="chezmoi_edit $XDG_CONFIG_HOME/fontconfig/conf.d/40-family-prefer.conf"
@@ -72,7 +72,7 @@ alias ktbrc="chezmoi_edit $XDG_CONFIG_HOME/kitty/tab_bar.conf"
 # config config status.showUntrackedFiles no
 
 # ==============================================
-# chezmoi-related aliases
+# chezmoi
 # ==============================================
 alias czm="chezmoi"
 alias czmr="chezmoi re-add"
@@ -100,7 +100,6 @@ alias czmrc="chezmoi edit-config-template"
 # ==============================================
 # alias js="joshuto"
 # alias jsrc="$EDITOR $XDG_CONFIG_HOME/joshuto/joshuto.toml"
-
 
 # ==============================================
 # hyprland config
@@ -130,6 +129,106 @@ alias rimewd="rime_edit '$RIME_USER_PATH/custom_phrase.txt'"
 #   --object-path /com/github/rime/ibus/Rime \
 #   --method com.github.rime.ibus.Rime.AsciiMode.GetAsciiMode"
 
+# ==============================================
+# wikispace
+# ==============================================
+alias wkstatus="systemctl status wikispace --user"
+alias wkstart="systemctl start wikispace --user"
+alias wkstop="systemctl stop wikispace --user"
+alias wkrestart="systemctl restart wikispace --user"
+alias wklog="journalctl --user --unit wikispace -f"
+
+# ==============================================
+# borg backup
+# ==============================================
+alias borgst="systemctl status borg-backup --user"
+alias borgex="chezmoi_edit $XDG_CONFIG_HOME/borg/excludes"
+alias borgls="borg list /mnt/backup/uzvg@archlinux"
+alias borglog="journalctl --user --unit borg-backup -f"
+
+# ==============================================
+# systemd-related
+# ==============================================
+alias sss="systemctl status"
+# clean journalctl file
+alias logclean="sudo journalctl --vacuum-size=200M"
+# Sync system time
+alias timesync="systemctl restart systemd-timesyncd.service"
+
+
+# ==============================================
+# hugo
+# ==============================================
+# alias hgs="hugo server -s $blogDir"
+# alias hgd="hugo server -s $blogDir -D"
+
+# ==============================================
+# Killing motherfucker QQ wechat & steam background jobs
+# ==============================================
+# alias fkqq="killall qq"
+# alias fkvv="flatpak kill com.valvesoftware.Steam"
+# alias fkwc="flatpak kill com.tencent.WeChat"
+
+# ==============================================
+# dae 管理快捷命令
+# ==============================================
+# alias ddstart='systemctl start dae.service'
+# alias ddstop='systemctl stop dae.service'
+# alias ddrestart='systemctl restart dae.service'
+# alias ddrld='systemctl reload dae.service'
+# alias ddst='systemctl status dae.service'
+# 查看实时日志 (显示最后50行并持续滚动)
+# alias ddlog='journalctl -u dae.service -f -n 50'
+# alias ddlg='journalctl -u dae.service -f -o cat'
+# alias daerc="chezmoi_edit $XDG_CONFIG_HOME/dae/config.dae"
+
+# ==============================================
+# mpd
+# ==============================================
+# mpd config
+alias mpdrc="chezmoi_edit $XDG_CONFIG_HOME/mpd/mpd.conf"
+alias rrc="chezmoi_edit $XDG_CONFIG_HOME/rmpc/config.ron"
+# lyrics download tool
+alias lyrics-tool="ZonyLrcTools.Cli"
+
+# ==============================================
+# others
+# ==============================================
+# gnome-terminal 主题修改👉https://gogh-co.github.io/Gogh/
+# alias gogh= bash -c "$(curl -sLo- https://git.io/vQgMr)"
+
+# starship config
+# alias ssrc="$EDITOR $HOME/.config/starship.toml"
+
+# tmux config
+# alias tmxcfg="$EDITOR $XDG_CONFIG_HOME/tmux/tmux.conf"
+
+# check nvidia status
+# alias nvss="watch -n 2 -d nvidia-smi"
+
+# shadowsocks
+# start shadowsocks service
+# alias ssstart="sudo sslocal -c /etc/shadowsocks/config.json -d start"
+# alias ssstop="sudo sslocal -c /etc/shadowsocks/config.json -d stop"
+
+# starship config
+# alias stcfg="$EDITOR $XDG_CONFIG_HOME/starship.toml"
+
+# edit ghostty config
+# alias gstrc="chezmoi_edit $XDG_CONFIG_HOME/ghostty/config.ghostty"
+
+# yazi config
+alias rarc="chezmoi_edit $XDG_CONFIG_HOME/yazi/yazi.toml"
+
+# jump to bottles directory
+alias btshome="cd .var/app/com.usebottles.bottles/data/bottles/bottles/Quark-Drive/drive_c/users/steamuser"
+
+# Replace sudo with sudo-rs
+# alias sudo="sudo-rs"
+
+# sync wallpapers
+alias wpsync="rclone sync $HOME/Pictures/Wallpapers OneDrive:Wallpapers --progress"
+
 # CloudServer
 # alias rmsv="ssh -i $HOME/.ssh/id_rsa -l $CloudUser $CloudServer"
 
@@ -141,92 +240,3 @@ alias dotfiles="lazygit -p $XDG_DATA_HOME/chezmoi"
 
 # uzvg zsh plugins dir
 alias uzpd="lazygit -p '$ZDOTDIR/plugins'"
-
-# systemctl control alias
-alias sss="systemctl status"
-alias wkstatus="systemctl status wikispace --user"
-alias wkstart="systemctl start wikispace --user"
-alias wkstop="systemctl stop wikispace --user"
-alias wkrestart="systemctl restart wikispace --user"
-alias wklog="journalctl --user --unit wikispace -f"
-
-# borg backup alias
-alias borgst="systemctl status borg-backup --user"
-alias borgex="chezmoi_edit $XDG_CONFIG_HOME/borg/excludes"
-alias borgls="borg list /mnt/backup/uzvg@archlinux"
-alias borglg="journalctl --user --unit borg-backup -f"
-
-# clean journalctl file
-alias logclean="sudo journalctl --vacuum-size=200M"
-
-# hugo shortcuts
-# alias hgs="hugo server -s $blogDir"
-# alias hgd="hugo server -s $blogDir -D"
-
-# gnome-terminal 主题修改👉https://gogh-co.github.io/Gogh/
-# alias gogh= bash -c "$(curl -sLo- https://git.io/vQgMr)"
-
-# starship config
-# alias ssrc="$EDITOR $HOME/.config/starship.toml"
-
-# tmux config
-# alias tmxcfg="$EDITOR $XDG_CONFIG_HOME/tmux/tmux.conf"
-
-# neofetch
-alias ff="fastfetch"
-
-# check nvidia status
-# alias nvss="watch -n 2 -d nvidia-smi"
-
-# shadowsocks
-# start shadowsocks service
-# alias ssstart="sudo sslocal -c /etc/shadowsocks/config.json -d start"
-# alias ssstop="sudo sslocal -c /etc/shadowsocks/config.json -d stop"
-#
-
-# starship config
-# alias stcfg="$EDITOR $XDG_CONFIG_HOME/starship.toml"
-
-# ==============================================
-# Killing motherfucker QQ wechat & steam background jobs
-# ==============================================
-# alias fkqq="killall qq"
-# alias fkvv="flatpak kill com.valvesoftware.Steam"
-# alias fkwc="flatpak kill com.tencent.WeChat"
-
-# jump to bottles directory
-alias btshome="cd .var/app/com.usebottles.bottles/data/bottles/bottles/Quark-Drive/drive_c/users/steamuser"
-
-# Replace sudo with sudo-rs
-# alias sudo="sudo-rs"
-
-# Sync system time
-alias timesync="systemctl restart systemd-timesyncd.service"
-
-# sync wallpapers
-alias wpsync="rclone sync $HOME/Pictures/Wallpapers OneDrive:Wallpapers --progress"
-
-# daed 管理快捷命令
-# alias ddstart='systemctl start dae.service'
-# alias ddstop='systemctl stop dae.service'
-# alias ddrestart='systemctl restart dae.service'
-# alias ddrld='systemctl reload dae.service'
-# alias ddst='systemctl status dae.service'
-# 查看实时日志 (显示最后50行并持续滚动)
-# alias ddlog='journalctl -u dae.service -f -n 50'
-# alias ddlg='journalctl -u dae.service -f -o cat'
-# alias daerc="chezmoi_edit $XDG_CONFIG_HOME/dae/config.dae"
-#
-
-# edit ghostty config
-# alias gstrc="chezmoi_edit $XDG_CONFIG_HOME/ghostty/config.ghostty"
-
-# yazi config
-alias rarc="chezmoi_edit $XDG_CONFIG_HOME/yazi/yazi.toml"
-
-# mpd config
-alias mpdrc="chezmoi_edit $XDG_CONFIG_HOME/mpd/mpd.conf"
-alias rrc="chezmoi_edit $XDG_CONFIG_HOME/rmpc/config.ron"
-
-# lyrics download tool
-alias lyrics-tool="ZonyLrcTools.Cli"
