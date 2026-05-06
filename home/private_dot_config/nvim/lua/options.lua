d19c73217e12f6177191cc454f5ae392f034d4c1
uzvg <1497911983@qq.com>
2026-05-06 21:38:53 +0800

    [nvim] enable cursorline

--- home/private_dot_config/nvim/lua/options.lua
+++ home/private_dot_config/nvim/lua/options.lua
@@ -1,6 +1,6 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- to enable cursorline!
+o.cursorlineopt ='both'
