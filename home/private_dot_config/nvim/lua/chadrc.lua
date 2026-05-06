c04a8aed67d5a43b9f2ae7ef93160390123745c1
uzvg <1497911983@qq.com>
2026-05-06 21:51:15 +0800

    [nvim] enable italic comment

--- home/private_dot_config/nvim/lua/chadrc.lua
+++ home/private_dot_config/nvim/lua/chadrc.lua
@@ -9,10 +9,11 @@ M.base46 = {
 	theme = "ayu_dark",
 	transparency = true,
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+    },
+	},
 }
 
 -- M.nvdash = { load_on_startup = true }
