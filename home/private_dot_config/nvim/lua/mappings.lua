a87b8a50cf8472ed1035dd53e0bd095210804e2f
uzvg <1497911983@qq.com>
2026-05-06 21:24:42 +0800

    [nvim] remapping some keys

--- home/private_dot_config/nvim/lua/mappings.lua
+++ home/private_dot_config/nvim/lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "Q", ":exit<CR>", { desc = "Exit neovim" })
+map("n", "S", ":w<CR>", { desc = "Save Changes" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
