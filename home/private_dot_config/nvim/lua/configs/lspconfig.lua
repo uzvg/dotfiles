97e45015f32e7982e444ab9988ae894acc92e1b3
uzvg <1497911983@qq.com>
2026-05-07 12:38:57 +0800

    [nvim] enable lsp for more languages

--- home/private_dot_config/nvim/lua/configs/lspconfig.lua
+++ home/private_dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,8 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "markdown_oxide", "lua_ls", "bashls", "pyright" }
+-- local servers = { "html", "cssls" }
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
