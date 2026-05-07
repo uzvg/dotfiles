4f101d79bb370d9bca924d51064aa9ba774aad4a
uzvg <1497911983@qq.com>
2026-05-07 15:58:14 +0800

    [nvim] chore format code

--- home/private_dot_config/nvim/lua/configs/lspconfig.lua
+++ home/private_dot_config/nvim/lua/configs/lspconfig.lua
@@ -3,10 +3,10 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "markdown_oxide", "lua_ls", "bashls", "pyright" }
 -- local servers = { "html", "cssls" }
 
-vim.lsp.config("bashls",{
-  filetypes = { 'bash', 'sh', 'zsh' },
+vim.lsp.config("bashls", {
+  filetypes = { "bash", "sh", "zsh" },
 })
 
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
