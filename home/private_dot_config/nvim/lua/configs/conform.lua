629d6ddf31d589c1e7c905253bc8749c6233c98d
uzvg <1497911983@qq.com>
2026-05-07 23:46:26 +0800

    [nvim] enable format on save

--- home/private_dot_config/nvim/lua/configs/conform.lua
+++ home/private_dot_config/nvim/lua/configs/conform.lua
@@ -13,11 +13,11 @@ local options = {
   --   },
   -- },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
