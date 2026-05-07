e3a28162acc184393b6f5a9640352eff9df2ca3b
uzvg <1497911983@qq.com>
2026-05-07 12:32:08 +0800

    [nvim] enable treesitter & add some languages

--- home/private_dot_config/nvim/lua/plugins/init.lua
+++ home/private_dot_config/nvim/lua/plugins/init.lua
@@ -22,13 +22,14 @@ return {
   -- test new blink
   { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc", "html", "css", "html",
+        "css", "python", "javascript", "typescript", "json",
+        "ron", "zsh", "bash",
+  		},
+  	},
+  },
 }
