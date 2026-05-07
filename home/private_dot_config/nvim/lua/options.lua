b3dabd9499bfe8b71468d7ed43889e33839e3846
uzvg <1497911983@qq.com>
2026-05-07 15:58:46 +0800

    [nvim] set shiftwidth 2

--- home/private_dot_config/nvim/lua/options.lua
+++ home/private_dot_config/nvim/lua/options.lua
@@ -13,3 +13,8 @@ vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 -- 默认不折叠（打开文件时所有代码都是展开的）
 -- 如果设为 0，打开文件时所有代码都会折叠起来
 vim.opt.foldlevel = 99
+
+vim.opt.shiftwidth = 2   -- 缩进时缩进的空格数
+vim.opt.tabstop = 2      -- 每一个制表符代表的空格数
+vim.opt.softtabstop = 2  -- 敲击 Tab 键时插入的空格数
+vim.opt.expandtab = true -- 将 Tab 转换为空格
