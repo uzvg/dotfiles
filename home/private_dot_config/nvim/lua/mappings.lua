c925fb5db04fee233564556ea04531cd7426329d
uzvg <1497911983@qq.com>
2026-05-07 12:59:07 +0800

    [nvim] remap qq to exit

--- home/private_dot_config/nvim/lua/mappings.lua
+++ home/private_dot_config/nvim/lua/mappings.lua
@@ -1,12 +1,10 @@
 require "nvchad.mappings"
-
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "Q", ":exit<CR>", { desc = "Exit neovim" })
+map("n", "qq", ":exit<CR>", { desc = "Exit neovim" })
 map("n", "S", ":w<CR>", { desc = "Save Changes" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
