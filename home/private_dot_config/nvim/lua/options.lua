c052dcf4b02bc7c4582e9996ce98502ecb536950
uzvg <1497911983@qq.com>
2026-05-07 12:40:25 +0800

    [nvim] enable treesitter foldmethod

--- home/private_dot_config/nvim/lua/options.lua
+++ home/private_dot_config/nvim/lua/options.lua
@@ -5,3 +5,11 @@ local o = vim.o
 -- to enable cursorline!
 -- o.cursorline = true
 o.cursorlineopt ='both'
+
+-- 开启 Tree-sitter 折叠
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- 默认不折叠（打开文件时所有代码都是展开的）
+-- 如果设为 0，打开文件时所有代码都会折叠起来
+vim.opt.foldlevel = 99
