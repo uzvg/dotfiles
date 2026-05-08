d42ab9abf5711b2a02eead986b0f684cbee7c6de
uzvg <1497911983@qq.com>
2026-05-08 19:01:23 +0800

    [style] reformat with stylua

--- home/private_dot_config/nvim/lua/options.lua
+++ home/private_dot_config/nvim/lua/options.lua
@@ -3,8 +3,8 @@ require "nvchad.options"
 local o = vim.o
 
 -- to enable cursorline!
--- o.cursorline = true
-o.cursorlineopt ='both'
+o.cursorline = true
+o.cursorlineopt = "both"
 
 -- 开启 Tree-sitter 折叠
 vim.opt.foldmethod = "expr"
@@ -14,7 +14,7 @@ vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 -- 如果设为 0，打开文件时所有代码都会折叠起来
 vim.opt.foldlevel = 99
 
-vim.opt.shiftwidth = 2   -- 缩进时缩进的空格数
-vim.opt.tabstop = 2      -- 每一个制表符代表的空格数
-vim.opt.softtabstop = 2  -- 敲击 Tab 键时插入的空格数
+vim.opt.shiftwidth = 2 -- 缩进时缩进的空格数
+vim.opt.tabstop = 2 -- 每一个制表符代表的空格数
+vim.opt.softtabstop = 2 -- 敲击 Tab 键时插入的空格数
 vim.opt.expandtab = true -- 将 Tab 转换为空格
